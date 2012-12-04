0f63de88c296cd98ad7d82d04105e7d3f49b5609
Eiji Takahashi <tak7iji@gmail.com>
2012-12-04 13:23:19 +0900

    avoid timeout

--- WebSocketTest3_Jetty/src/main/java/jp/co/ntts/sample/NoEchoServlet.java
+++ WebSocketTest3_Jetty/src/main/java/jp/co/ntts/sample/NoEchoServlet.java
@@ -9,21 +9,23 @@ import org.eclipse.jetty.websocket.WebSocketServlet;
 public class NoEchoServlet extends WebSocketServlet {
     private static final Logger logger = Logger.getLogger(NoEchoServlet.class.getName());
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public WebSocket doWebSocketConnect(HttpServletRequest hsr, String string) {
-		return new WebSocket.OnTextMessage() {
+    public WebSocket doWebSocketConnect(HttpServletRequest hsr, String string) {
+        return new WebSocket.OnTextMessage() {
 
-			public void onMessage(String data) {
-			    if(data.equals("hello")) return;
-	            logger.info(data+","+System.currentTimeMillis());
-			}
+            public void onMessage(String data) {
+                if(data.equals("hello")) return;
+                logger.info(data+","+System.currentTimeMillis());
+            }
 
-			public void onOpen(Connection connection) {
-			}
+            public void onOpen(Connection connection) {
+                connection.setMaxIdleTime(0);
+            }
 
-			public void onClose(int closeCode, String message) {
-			}
-		};
-	}
+            public void onClose(int closeCode, String message) {
+                logger.info("Connection clonsed: "+closeCode);
+            }
+        };
+    }
 }
